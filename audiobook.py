790f58455f6fc38c17201d29e251aafd46b9082f
Astro14 <71722036+astronaut14@users.noreply.github.com>
2023-06-21 16:45:13 +0530

    Update audiobook.py

--- audiobook.py
+++ audiobook.py
@@ -6,8 +6,8 @@ pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 
 speaker = pyttsx3.init()
-for num in range(1, pages):
+for num in range(10, pages):
     page = pdfReader.getPage(num)
-    text = page.extractText()
+    text = page.extractText(int=num)
     speaker.say(text)
     speaker.runAndWait()
